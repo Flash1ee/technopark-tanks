1ddb990dccb34e2e7d06944fbdbfe26a807f9341
Max Dudnik <maxdudnik@mail.ru>
2020-12-08 14:51:58 +0000

    Revert "Merge branch 'Dev' into 'Maks'"
    This reverts commit 8dd284bd3c2b290f02a223cda66efb47083279ff

--- project/includes/objects.h
+++ project/includes/objects.h
@@ -38,29 +38,23 @@ class Object {
 };
 
 class Bullet : public Object {
-public:
-    Bullet(Level& mapObj, sf::String textureFile, sf::String soundFile, sf::IntRect rect, sf::Vector2f pos,
-           float speed, Direction dir, int life)
-        : Object(textureFile, rect, pos, speed, dir), m_life(life) {
-        m_objects = mapObj.GetAllObjects("solid");
+   public:
+    Bullet(sf::String textureFile, sf::String soundFile, sf::IntRect rect, sf::Vector2f pos,
+           float speed, Direction dir)
+        : Object(textureFile, rect, pos, speed, dir){
             if (!m_buffer.loadFromFile(soundFile)) {
                 throw std::exception();
             }
             m_sound.setBuffer(m_buffer);
         };
     void move(float time);
-    void checkCollisionsMap();
     void sound();
-    int getLife() const;
-private:
-    int m_life;
 };
 
 class Tank : public Object {  //класс любого танка
    protected:
     // sf::String name;
     int hp;
-    std::vector<std::shared_ptr<Bullet>> m_bullets;
     // Direction dir;
     bool shot;
     // size_t armor;
