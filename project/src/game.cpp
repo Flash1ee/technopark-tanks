100bd7f27cc51da4664e3540c23b26b8a85be4d8
Maksongold <maxdudnik@mail.ru>
2020-12-18 01:54:09 +0300

    "Menu is ready!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"

--- project/src/game.cpp
+++ project/src/game.cpp
@@ -11,7 +11,6 @@
 
 int main(int argc, char* argv[]) {
     int gamePlaying = 1;
-    sf::RenderWindow menuWindow(sf::VideoMode(1024, 768), std::string("Menu"), sf::Style::None);
     while (gamePlaying) {
     // std::string window_title = std::string("Tanks") + std::string(argv[1]);
         // std::string window_title = std::string("Tanks");
@@ -19,13 +18,14 @@ int main(int argc, char* argv[]) {
         std::string player_skin(OBJECT_IMAGE);
         std::string map_skin(MAP_ONE);
         std::string server_ip = sf::IpAddress::getLocalAddress().toString();
+        sf::RenderWindow window(sf::VideoMode(1024, 768), std::string("Tanks"), sf::Style::Fullscreen);
 
         // GameSession game_session(window_title, map_skin, player_skin, true,
         // server_ip, PORT);
         // sf::RenderWindow menuWindow(sf::VideoMode(1024, 768), std::string("Menu"), sf::Style::None);
         // std::cout << "GAY"<<'\n';
-        Menu mainMenu(0, menuWindow);
-        gamePlaying = mainMenu.menuSwitcher(menuWindow, map_skin, player_skin, server_ip);
+        Menu mainMenu(0, window);
+        gamePlaying = mainMenu.menuSwitcher(window, map_skin, player_skin, server_ip);
         // std::cout << gamePlaying << '\n';
         // switch (result)
         // {
