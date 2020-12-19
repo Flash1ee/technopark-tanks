3c18055052326f077b102a7b0305063aee7cdb7d
Maksongold <maxdudnik@mail.ru>
2020-12-20 01:08:31 +0300

    "Added health HEARTS <3<3<3"

--- project/includes/statistics.h
+++ project/includes/statistics.h
@@ -9,12 +9,20 @@
 #include <SFML/Audio.hpp>
 #include "objects.h"
 #define FONT "../resources/19888.ttf"
+#define HEALTH "../resources/health.png"
+
+const inline sf::IntRect full(50, 23, 121, 121);
+const inline sf::IntRect half(195, 23, 121, 121);
+const inline sf::IntRect empty(340, 23, 121, 121);
 
 class Statistic {
     private:
+        sf::Image health_img;
+        sf::Texture health_text;
         sf::Font font;
         std::vector <sf::Text> text;
         sf::Time time;
+        std::vector <sf::Sprite> health;
     public:
         Statistic(sf::RenderWindow& window);
         void update(sf::RenderWindow& window, int hp, double time);
