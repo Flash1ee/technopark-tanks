cf39feb35522e4ea1d847329ba736e9389819454
Alexey Bakharew <alex.bakharew@gmail.com>
2020-12-23 22:56:56 +0300

    changes

--- project/src/client.cpp
+++ project/src/client.cpp
@@ -20,7 +20,7 @@ bool Client::connectToServer() {
     //std::cout << m_ip_adress.toString() << " " << m_port << std::endl;
     m_ip_adress = SERVER_IP;
     m_port = PORT;
-    
+
     if (m_socket->connect(m_ip_adress, m_port) == sf::Socket::Done)
     {
         std::cout << "connected!" << std::endl;
